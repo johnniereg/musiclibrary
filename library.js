c2f826a88c57c2406d26ca1877c5b4b59132699b
Johnnie Regalado <johnniereg@gmail.com>
2017-11-01 23:44:39 +0000

    Got first three functions working.

--- library.js
+++ library.js
@@ -21,7 +21,7 @@ var library = {
                       tracks: ["t03"]
                     }
              }
-}
+};
 
 // FUNCTIONS TO IMPLEMENT:
 
@@ -29,19 +29,37 @@ var library = {
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
 
-var printPlaylists = function () {
+var printPlaylists = function (musicLibrary) {
+  var thePlaylists = musicLibrary["playlists"];
+  for (var eachList in thePlaylists) {
+    var listItems = thePlaylists[eachList];
+    var numOfTracks = listItems.tracks.length;
+    console.log(listItems["id"] + ": " + listItems["name"] + " - " + numOfTracks + " tracks");
+  }
+};
 
-}
+// THIS ONE WORKS
+// Uncomment lower line to test
+// printPlaylists(library);
+
+// // prints a list of all tracks, in the form:
+// // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
+// // t02: Model View Controller by James Dempsey (WWDC 2003)
+// // t03: Four Thirty-Three by John Cage (Woodstock 1952)
+
+var printTracks = function (musicLibrary) {
+  var allTracks = musicLibrary["tracks"];
+  for (var track in allTracks) {
+    var tracks = allTracks[track];
+    console.log(tracks["id"] + ": " + tracks["name"] + " by " + tracks["artist"] + " (" + tracks["album"] + ")");
+  }
+};
+
+// THIS ONE WORKS
+// Uncomment lower line to test
+// printTracks(library);
 
 
-// prints a list of all tracks, in the form:
-// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
-// t02: Model View Controller by James Dempsey (WWDC 2003)
-// t03: Four Thirty-Three by John Cage (Woodstock 1952)
-
-var printTracks = function () {
-
-}
 
 
 // prints a list of tracks for a given playlist, in the form:
@@ -50,15 +68,35 @@ var printTracks = function () {
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 
 var printPlaylist = function (playlistId) {
+  // This code prints the specified playlist details
+  var theID = playlistId;
+  var playlists = library["playlists"];
+  var allTracks = library["tracks"];
+  var selectedPlaylist = playlists[theID];
+  var numOfTracks = selectedPlaylist.tracks.length;
+  console.log(selectedPlaylist["id"] + ": " + selectedPlaylist["name"] + " - " + numOfTracks + " tracks");
 
-}
+  // This code prints the playlist track details
+  var tracksInPlaylist = selectedPlaylist["tracks"];
+
+  for (i = 0; i < tracksInPlaylist.length; i++) {
+    var specificTrack = tracksInPlaylist[i];
+    var trackDetails = allTracks[specificTrack];
+    console.log(trackDetails["id"] + ": " + trackDetails["name"] + " by " + trackDetails["artist"] + " (" + trackDetails["album"] + ")");
+  }
+};
+
+// THIS ONE WORK
+// Uncomment lower lines to test
+// printPlaylist ("p01");
+// printPlaylist ("p02");
 
 
 // adds an existing track to an existing playlist
 
 var addTrackToPlaylist = function (trackId, playlistId) {
 
-}
+};
 
 
 // generates a unique id
@@ -86,7 +124,7 @@ var addPlaylist = function (name) {
 // STRETCH:
 // given a query string string, prints a list of tracks
 // where the name, artist or album contains the query string (case insensitive)
-// tip: use "string".search("tri") 
+// tip: use "string".search("tri")
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
 
 var printSearchResults = function(query) {
